d0ba54dfcf64f748e7c2ebde0b257aaa8a80abf5
Christian Lazo <christianlazo@Christians-MBP.lan>
2017-05-21 17:31:27 -0700

    User auth

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'user sign up' do
+  it 'can sign up with user attributes' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: 'test123@test.com'
+    fill_in 'Password', with: 'test123'
+    fill_in 'Password Confirmation', with: 'test123'
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Snow'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content("Welcome")
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+      @user = User.create(email: "test123@test.com", password: "test123", password_confirmation: "test123", first_name: "John", last_name: "Snow")
+    end
+
+    it "can create users" do
+      expect(@user).to be_valid
+    end
+
+    it "cannot be created without first and last name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).not_to be_valid
+    end
+
+    it "should ensure all emails are unique" do
+      duplicate_email_user = User.create(email: "test123@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+      expect(duplicate_email_user).not_to be_valid
+    end
+  end
 end
