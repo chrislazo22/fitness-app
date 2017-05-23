22bcf602dcff457090010927aa4c093de687e27d
Christian Lazo <christianlazo@Christians-MBP.lan>
2017-05-22 18:38:02 -0700

    Homepage modifications

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -4,6 +4,8 @@ describe 'navigate' do
   describe 'homepage' do
     it 'can be reached successfully' do
       visit root_path
+      expect(page).to have_content("Current Workout Plan")
+      expect(page).to have_content("Create New Workout Plan")
       expect(page.status_code).to eq(200)
     end
   end
