5c232865a092d6f531df540bef4518ecd4ce30ac
Christian Lazo <christianlazo@Christians-MBP.lan>
2017-05-22 20:43:33 -0700

    Updated new workout plan form

--- app/controllers/workout_plans_controller.rb
+++ app/controllers/workout_plans_controller.rb
@@ -1,2 +1,8 @@
 class WorkoutPlansController < ApplicationController
+  def index
+  end
+
+  def new
+    @plan = WorkoutPlan.new
+  end
 end
---
+++ spec/features/workoutplan_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'navigate' do
+  describe 'index' do
+    it 'can be reached successfully and fill out form' do
+      visit workout_plans_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe 'form' do
+    it 'has a fitness questionnaire' do
+      visit new_workout_plan_path
+
+      # fill_in 'Gender', with: "Female"
+      # fill_in 'plan[age]', with: "21"
+      # fill_in 'Hegiht', with: "5 9"
+      # fill_in 'fitness_level', with: "Beginner"
+      # fill_in 'Goal', with: "Healthy Lifestyle"
+
+      click_on "Submit"
+
+      expect(page).to have_content("Here is your new Workout plan")
+    end
+  end
+end
