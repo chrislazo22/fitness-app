f6e80da24fcf5436343b110c36c7b7e931a6549c
Christian Lazo <lazochristian.cl@gmail.com>
2017-07-29 12:33:26 -0700

    Seed workout data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'workouts.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+csv.each do |row|
+  workout_hash = row.to_hash
+  workout = Workout.where(id: workout_hash["id"])
+
+  if workout.count == 1
+    workout.first.update_attributes.(workout_hash)
+  else
+    Workout.create!(workout_hash)
+  end
+end
+
+puts "#{Workout.count}"
